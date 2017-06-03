1cae593130b6b3185ff4b8fac400971ff12ee990
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-03 15:13:38 +0300

    create asp.net core .net framework based server

---
+++ RRPiCam/RRPiCam.Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+
+namespace RRPiCam.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            IPProvider.ShowIP();
+            var host = new WebHostBuilder()
+                .UseUrls("http://0.0.0.0:33333")
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class IPProvider
+    {
+        public static void ShowIP()
+        {
+            Console.WriteLine("-------------------------");
+            var strHostName = Dns.GetHostName();
+            Console.WriteLine($"Host Name: {strHostName}");
+            foreach (IPAddress address in Dns.GetHostAddresses(strHostName))
+            {
+                Console.WriteLine(address);
+            }
+            Console.WriteLine("-------------------------");
+        }
+    }
+}
